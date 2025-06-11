2ef66232c34dccc8e8d25b53048d393e5ff4aedf
Hazim Bakar <hazimbaa@gmail.com>
2025-06-11 15:23:29 +0800

    add delete route

--- routes/user.js
+++ routes/user.js
@@ -68,4 +68,22 @@ router.patch("/updateUser/:id", async (req, res) => {
   }
 });
 
+router.delete("/deleteUser/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ message: "User ID is required." });
+  }
+
+  try {
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting user", error });
+  }
+});
+
 export default router;
