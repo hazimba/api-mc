f1e97cffb4b4ab7acefd2cb8269be6800f742eaf
Hazim Bakar <hazimbaa@gmail.com>
2025-06-11 16:45:43 +0800

    add controller and employee schema

---
+++ model/employee.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("Employee", employeeSchema);
