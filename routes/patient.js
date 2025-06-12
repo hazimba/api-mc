e743b3a1f7cbc1387ce9df73e6b3d2147c2cfbd6
Hazim Bakar <hazimbaa@gmail.com>
2025-06-12 09:28:05 +0800

    add route employee

--- routes/patient.js
+++ routes/patient.js
@@ -6,11 +6,15 @@ import {
   deletePatient,
 } from "../controller/patient.js";
 
+import { getEmployees, addEmployee } from "../controller/employee.js";
+
 const router = express.Router();
 
 router.get("/getPatients", getPatients);
 router.post("/addPatient", addPatient);
 router.patch("/updatePatient/:id", updatePatient);
 router.delete("/deletePatient/:id", deletePatient);
+router.get("/getEmployees", getEmployees);
+router.post("/addEmployee", addEmployee);
 
 export default router;
